bdf437bcf2dcf12107a6e7af978ace2a44d4bcf1
SuperAme <beatamez@hotmail.com>
2019-03-11 20:04:43 -0600

    cambios finales

--- app/controllers/pages.php
+++ app/controllers/pages.php
@@ -2,22 +2,17 @@
     class pages extends Controller{
         public function __construct(){
             //echo "Controlador paginas cargado";
-            $this->articuloModelo = $this->modelo('Articulo');
+            //$this->articuloModelo = $this->modelo('Articulo');
         }
         public function index(){
-            $articulos = $this->articuloModelo->obtenerArticulos();
+            //$articulos = $this->articuloModelo->obtenerArticulos();
             
             $datos = [
                 'titulo' => 'Welcome to my MVC',
-                'articulos' => $articulos
+                
             ];
             $this->view('Pages/ini',$datos);
         }
-        public function articulo($num_registro){
-            $this->view('Pages/articulo');
-        }
-        public function actualizar($num_registro){
-            echo $num_registro;
-        }
+        
     }
 ?>
--- app/views/Pages/ini.php
+++ app/views/Pages/ini.php
@@ -3,10 +3,5 @@
 <p><?php
     echo $datos['titulo'];
 ?></p>
-<ul>
-    <?php foreach($datos['articulos'] as $articulos): ?>
-        <li><?php echo $articulos->titulo; ?></li>
-    <?php endforeach; ?>
-    
-</ul>
+
 <?php require RUTA_APP.'/views/inc/footer.php'; ?>
